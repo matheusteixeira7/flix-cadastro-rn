2abd1b0fba57b7ac06bf484c4ae5ce35320822a0
matheusteixeira7 <matheus.teixeira7@gmail.com>
2022-01-29 12:59:24 -0300

    feat: add global styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+export default {
+  colors: {
+    primary: '#D33C32',
+
+    secondary: '#585666',
+
+    success: '#12A454',
+    success_light: 'rgba(18, 165, 84, 0.3)',
+
+    attention: '#e83f5b',
+    attention_light: 'rgba(232, 63, 91, 0.3)',
+
+    white: '#fff',
+
+    gray_100: '#EEEEEE',
+    gray_200: '#CCCCCC',
+    gray_300: '#999999',
+    gray_400: '#666666',
+    gray_500: '#333333',
+    gray_600: '#222222',
+    gray_700: '#111111',
+    gray_800: '#000000',
+
+    title: '#585666',
+    subtitle: '#706E7A',
+
+    transparent: 'rgba(0, 0, 0, 0)',
+  },
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  },
+}
