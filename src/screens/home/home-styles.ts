64a68bd698ba9f45259ec371d1c8d74e37222381
matheusteixeira7 <matheus.teixeira7@gmail.com>
2022-01-29 16:21:24 -0300

    feat: add registered users title

--- src/screens/home/home-styles.ts
+++ src/screens/home/home-styles.ts
@@ -1,7 +1,16 @@
+import { RFValue } from 'react-native-responsive-fontsize'
 import styled from 'styled-components/native'
 
 export const Container = styled.View``
 
 export const PageContainer = styled.View`
-  margin: 0 24px;
+  margin: -40px 24px;
+`
+
+export const RegisteredUsersTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.semibold};
+  font-size: ${RFValue(20)}px;
+  color: ${({ theme }) => theme.colors.title};
+
+  margin: 32px 0 24px;
 `
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
 import { Registered } from '../../components/registered/registered'
+import { Separator } from '../../components/separator/separator'
 import { Topbar } from '../../components/topbar/topbar'
-import { Container, PageContainer } from './home-styles'
+import { Container, PageContainer, RegisteredUsersTitle } from './home-styles'
 
 export const Home = () => {
   return (
     <Container>
       <Topbar />
+
       <PageContainer>
         <Registered />
+        <RegisteredUsersTitle>Usuários Cadastrados</RegisteredUsersTitle>
+        <Separator />
       </PageContainer>
     </Container>
   )
